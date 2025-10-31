4b4987ed4efb466383ba84adccea257360660136
Frost <sniper321kc@gmail.com>
2025-10-31 18:25:01 -0500

    Initial

---
+++ src/main/Entity.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Entity {
+	
+	private String name;
+	
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
