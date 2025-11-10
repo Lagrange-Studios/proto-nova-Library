06638fd48365e474fd8c718ce4cee494654acc59
SDPowerStroke <cole.howdeshell@gmail.com>
2025-11-09 20:26:52 -0600

    fix it frost!!

--- src/main/java/library/Entity.java
+++ src/main/java/library/Entity.java
@@ -1,13 +1,5 @@
 package library;
 
 public class Entity {
-	private String name;
-	
-	public void setName(String newName) {
-		name = newName;
-	}
-	
-	public String getName() {
-		return name;
-	}
+	public String name;
 }
