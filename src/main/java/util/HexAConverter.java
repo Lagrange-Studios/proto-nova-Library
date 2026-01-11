995bc2001359dee73f074361af4b3a6b9813e32d
Frost <sniper321kc@gmail.com>
2026-01-10 22:09:35 -0600

    Remvoed HEXA

--- src/main/java/util/HexAConverter.java
+++ src/main/java/util/HexAConverter.java
@@ -7,8 +7,7 @@ public class HexAConverter {
 		int r = Integer.valueOf(hex.substring(0, 2), 16);
 		int g = Integer.valueOf(hex.substring(2, 4), 16);
 		int b = Integer.valueOf(hex.substring(4, 6), 16);
-		int a = Integer.valueOf(hex.substring(6, 8), 16);
 
-		return new Color(r, g, b, a);
+		return new Color(r, g, b);
 	}
 }
