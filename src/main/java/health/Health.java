e94e4bb0c2c703b7f9ad853e4b6259179161bc97
Cole <Cole@DESKTOP-D5126DM.attlocal.net>
2026-06-27 12:30:40 -0500

    Health system more universal

--- src/main/java/health/Health.java
+++ src/main/java/health/Health.java
@@ -4,25 +4,31 @@ import protonova.protobuf.EntityProto.Entity;
 
 public class Health {
 	
-	public enum HealthState {
+	public enum TraumaState {
 	    HEALTHY(0), 
-	    MINOR_INJURIES(15), 
+	    MINOR_INJURIES(15),
 	    INJURED(40),
-		SEVERLY_INJURED(70),
+	    SEVERELY_INJURED(70),
 		MORTALLY_WOUNDED(90),
 		CRITICAL(100),
-		NEAR_DEATH(150),
-		DEAD(200);
+		// Health Percent After Critical Threashold
+		NEAR_DEATH(50),
+		DEAD(100);
 
-	    // At what point the Health State starts
-	    private final double percent;
+	    // At what point the Trauma State starts
+	    private final double trauma;
 
-	    HealthState(int percent) {
-	        this.percent = percent;
+	    TraumaState(int trauma) {
+	        this.trauma = trauma;
 	    }
 
-	    public double getPercent() {
-	        return this.percent;
+	    public double getTraumaPercent() {
+	        return this.trauma;
+	    }
+	    
+	    public double getTraumaPercentAsDecimal() {
+	    	//returns decimal
+	        return this.trauma / 100;
 	    }
 	}
 	
@@ -37,4 +43,37 @@ public class Health {
 		return totalDamage;
 		
 	}
+	
+	public static TraumaState getTraumaStateFromEntity(Entity entity) {
+		if (!entity.getAlive()) {
+			return TraumaState.DEAD;
+		}
+		double entityDamage = getDamage(entity);
+		
+		if (entityDamage >= entity.getCritHealth()) {
+			double healthDiff = entity.getMaxHealth() - entity.getCritHealth();
+			double damageAfterCrit = entityDamage - entity.getCritHealth();
+			if (entityDamage >= entity.getMaxHealth()) {
+				return TraumaState.DEAD;
+			} else if (healthDiff - TraumaState.NEAR_DEATH.getTraumaPercentAsDecimal() <= damageAfterCrit) {
+				return TraumaState.NEAR_DEATH;
+			} else {
+				return TraumaState.CRITICAL;
+			}
+		} else {
+			if (entityDamage >= entity.getCritHealth() * TraumaState.MORTALLY_WOUNDED.getTraumaPercentAsDecimal()) {
+				return TraumaState.MORTALLY_WOUNDED;
+			} else if (entityDamage >= entity.getCritHealth() * TraumaState.SEVERELY_INJURED.getTraumaPercentAsDecimal()) {
+				return TraumaState.SEVERELY_INJURED;
+			} else if (entityDamage >= entity.getCritHealth() * TraumaState.INJURED.getTraumaPercentAsDecimal()) {
+				return TraumaState.INJURED;
+			} else if (entityDamage >= entity.getCritHealth() * TraumaState.MINOR_INJURIES.getTraumaPercentAsDecimal()) {
+				return TraumaState.MINOR_INJURIES;
+			} else {
+				return TraumaState.HEALTHY;
+			}
+		}
+	}
+	
+	
 }
