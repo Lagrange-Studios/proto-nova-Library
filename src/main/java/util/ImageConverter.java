f2a1f8aba83c49f03907bd849dfe297ff09a43f6
SDPowerStok3 <s78192@parkhill.k12.mo.us>
2026-09-10 10:16:55 -0500

    Images can now support bigger than 64 as long as they are squares.

--- src/main/java/util/ImageConverter.java
+++ src/main/java/util/ImageConverter.java
@@ -7,14 +7,25 @@ public class ImageConverter {
     public static BufferedImage getSprite(BufferedImage sheet, int imageNumber, int size) {
         // x and y are the top-left coordinates of the desired sprite within the sheet
         // width and height are the dimensions of the sprite
-    	int y = 0;
-    	int x = 0;
-    	if (imageNumber > 2) {
-    		x = imageNumber - 3;
-    		y = 1;
-    	} else {
-    		x = imageNumber - 1;
-    	}
-        return sheet.getSubimage(x*size, y*size, size, size);
+        int sheetWidth = sheet.getWidth();
+        int sheetHeight = sheet.getHeight();
+        
+        int spritesPerRow = sheetWidth / size;
+        int spritesPerColumn = sheetHeight / size;
+        
+        int y = 0;
+        int x = 0;
+        if (imageNumber > 2) {
+            x = imageNumber - 3;
+            y = 1;
+        } else {
+            x = imageNumber - 1;
+        }
+        
+        if (x >= spritesPerRow || y >= spritesPerColumn) {
+            throw new IllegalArgumentException("Sprite number " + imageNumber + " is out of bounds for sheet size " + sheetWidth + "x" + sheetHeight + ". Sheet can only hold " + (spritesPerRow * spritesPerColumn) + " sprites.");
+        }
+        
+        return sheet.getSubimage(x * size, y * size, size, size);
     }
 }
