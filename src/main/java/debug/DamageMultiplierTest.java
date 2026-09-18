d1af5df90d158d6e1f876aa3f5d83422bd9be677
FrostSniperDude <sniper321kc@gmail.com>
2026-09-18 11:11:02 -0500

    Added damage multiplier

--- src/main/java/debug/DamageMultiplierTest.java
+++ src/main/java/debug/DamageMultiplierTest.java
@@ -1,7 +1,6 @@
 package debug;
 
 import java.util.ArrayList;
-
 import protonova.protobuf.DamageProto.Damage;
 import protonova.protobuf.DamageProto.DamageMultiplier;
 import protonova.protobuf.EntityProto.Entity;
@@ -9,32 +8,45 @@ import util.DamageMultiplierUtil;
 
 public class DamageMultiplierTest {
 
-	public static void main(String[] args) {
-		Entity entity = Entity.newBuilder()
-				.setDamage(Damage.newBuilder().setDamageMultiplier(DamageMultiplier.newBuilder()
-						.setAsphyxiation(2f)
-						.setBleeding(2f)
-						.setBurn(2f)
-						.setGenetic(2f).build()).build()).build();
-		
-		Entity entity2 = Entity.newBuilder()
-				.setDamage(Damage.newBuilder().setDamageMultiplier(DamageMultiplier.newBuilder()
-						.setAsphyxiation(3f)
-						.setBleeding(3f)
-						.setBurn(3f)
-						.setStructural(1.5f).build()).build()).build();
-		ArrayList<Entity> list = new ArrayList<>();
-		list.add(entity2);
-		
-		DamageMultiplier multi = DamageMultiplierUtil.getTotalDamageMultiplier(entity, list);
-		
-		System.out.println("Brute: "+multi.getBrute());
-		System.out.println("burn: "+multi.getBurn());
-		System.out.println("struc: "+multi.getStructural());
-		System.out.println("genetic: "+multi.getGenetic());
-		System.out.println("Asphyxiation: "+multi.getAsphyxiation());
-		System.out.println("Toxin: "+multi.getToxin());
-		System.out.println("Bleeding: "+multi.getBleeding());
-	}
+  public static void main(String[] args) {
+    Entity entity =
+        Entity.newBuilder()
+            .setDamage(
+                Damage.newBuilder()
+                    .setDamageMultiplier(
+                        DamageMultiplier.newBuilder()
+                            .setAsphyxiation(2f)
+                            .setBleeding(2f)
+                            .setBurn(2f)
+                            .setGenetic(2f)
+                            .build())
+                    .build())
+            .build();
 
+    Entity entity2 =
+        Entity.newBuilder()
+            .setDamage(
+                Damage.newBuilder()
+                    .setDamageMultiplier(
+                        DamageMultiplier.newBuilder()
+                            .setAsphyxiation(3f)
+                            .setBleeding(3f)
+                            .setBurn(3f)
+                            .setStructural(1.5f)
+                            .build())
+                    .build())
+            .build();
+    ArrayList<Entity> list = new ArrayList<>();
+    list.add(entity2);
+
+    DamageMultiplier multi = DamageMultiplierUtil.getTotalDamageMultiplier(entity, list);
+
+    System.out.println("Brute: " + multi.getBrute());
+    System.out.println("burn: " + multi.getBurn());
+    System.out.println("struc: " + multi.getStructural());
+    System.out.println("genetic: " + multi.getGenetic());
+    System.out.println("Asphyxiation: " + multi.getAsphyxiation());
+    System.out.println("Toxin: " + multi.getToxin());
+    System.out.println("Bleeding: " + multi.getBleeding());
+  }
 }
--- src/main/java/util/DamageMultiplierUtil.java
+++ src/main/java/util/DamageMultiplierUtil.java
@@ -1,38 +1,40 @@
 package util;
 
-import java.util.ArrayList;
-import java.util.Map.Entry;
-
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.Descriptors.FieldDescriptor;
-
+import java.util.ArrayList;
 import protonova.protobuf.DamageProto.DamageMultiplier;
 import protonova.protobuf.EntityProto.Entity;
 
 public class DamageMultiplierUtil {
 
-	public static DamageMultiplier getTotalDamageMultiplier(Entity entity, ArrayList<Entity> modifiers) {
-		
-		DamageMultiplier.Builder base = entity.getDamage().getDamageMultiplier().toBuilder();
-		
-		for (Entity gear : modifiers) {
-			base = multiply(base, gear.getDamage().getDamageMultiplier());
-		}
-		
-		return base.build();
-	}
-	
-	private static DamageMultiplier.Builder multiply(DamageMultiplier.Builder damage1, DamageMultiplier damage2) {
-		
-		for (FieldDescriptor field  : DamageMultiplier.getDescriptor().getFields()) {
-			if (field.getJavaType().equals(Descriptors.FieldDescriptor.JavaType.FLOAT)) {
-				if (damage1.hasField(field) && damage2.hasField(field)) damage1.setField(field, (float) damage1.getField(field) * (float) damage2.getField(field));
-				else if (damage1.hasField(field)) continue; // do nothing cause its currently in the builder already
-				else if (damage2.hasField(field)) damage1.setField(field, damage2.getField(field));
-				else damage1.setField(field, 1f);
-			}
-		}
-		
-		return damage1;
-	}
+  public static DamageMultiplier getTotalDamageMultiplier(
+      Entity entity, ArrayList<Entity> modifiers) {
+
+    DamageMultiplier.Builder base = entity.getDamage().getDamageMultiplier().toBuilder();
+
+    for (Entity gear : modifiers) {
+      base = multiply(base, gear.getDamage().getDamageMultiplier());
+    }
+
+    return base.build();
+  }
+
+  private static DamageMultiplier.Builder multiply(
+      DamageMultiplier.Builder damage1, DamageMultiplier damage2) {
+
+    for (FieldDescriptor field : DamageMultiplier.getDescriptor().getFields()) {
+      if (field.getJavaType().equals(Descriptors.FieldDescriptor.JavaType.FLOAT)) {
+        if (damage1.hasField(field) && damage2.hasField(field))
+          damage1.setField(
+              field, (float) damage1.getField(field) * (float) damage2.getField(field));
+        else if (damage1.hasField(field))
+          continue; // do nothing cause its currently in the builder already
+        else if (damage2.hasField(field)) damage1.setField(field, damage2.getField(field));
+        else damage1.setField(field, 1f);
+      }
+    }
+
+    return damage1;
+  }
 }
